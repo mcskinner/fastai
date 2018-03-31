c524d3454e8e8642f6643ccfb5f6059b55086cee
mcskinner <git@mcskinner.com>
2018-03-30 18:46:43 -0700

    grok dataloader.py

--- fastai/dataloader.py
+++ fastai/dataloader.py
@@ -1,26 +1,13 @@
-import torch, queue
+import torch
 from torch.utils.data.sampler import SequentialSampler, RandomSampler, BatchSampler
 from .imports import *
 from .core import *
-import collections,sys,traceback,threading
-
-string_classes = (str, bytes)
-
-
-def get_tensor(batch, pin):
-    if isinstance(batch, (np.ndarray, np.generic)):
-        batch = T(batch).contiguous()
-        return batch.pin_memory() if pin else batch
-    elif isinstance(batch, string_classes): return batch
-    elif isinstance(batch, collections.Mapping):
-        return {k: get_tensor(sample, pin) for k, sample in batch.items()}
-    elif isinstance(batch, collections.Sequence):
-        return [get_tensor(sample, pin) for sample in batch]
-    raise TypeError("batch must contain numbers, dicts or lists; found {}"
-                     .format(type(batch)))
+import collections
 
 
 class DataLoader(object):
+    """Split a dataset into batches, and then be iterable over those batches."""
+
     def __init__(self, dataset, batch_size=1, shuffle=False, sampler=None, batch_sampler=None, pad_idx=0,
                  num_workers=None, pin_memory=False, drop_last=False, pre_pad=True,
                  transpose=False, transpose_y=False):
@@ -41,12 +28,29 @@ class DataLoader(object):
                 sampler = RandomSampler(dataset) if shuffle else SequentialSampler(dataset)
             batch_sampler = BatchSampler(sampler, batch_size, drop_last)
 
-        self.sampler = sampler
         self.batch_sampler = batch_sampler
 
-    def __len__(self): return len(self.batch_sampler)
+    def __len__(self):
+        return len(self.batch_sampler)
 
-    def jag_stack(self, b):
+    def __iter__(self):
+        if self.num_workers==0:
+            for batch in map(self.get_batch, iter(self.batch_sampler)):
+                yield get_tensor_(batch, self.pin_memory)
+        else:
+            with ThreadPoolExecutor(max_workers=self.num_workers) as e:
+                # avoid py3.6 issue where queue is infinite and can result in memory exhaustion
+                for c in chunk_iter(iter(self.batch_sampler), self.num_workers*10):
+                    for batch in e.map(self.get_batch, c):
+                        yield get_tensor_(batch, self.pin_memory)
+
+    def get_batch(self, indices):
+        res = self.np_collate_([self.dataset[i] for i in indices])
+        if self.transpose:   res[0] = res[0].T
+        if self.transpose_y: res[1] = res[1].T
+        return res
+
+    def stack_jagged_(self, b):
         if len(b[0].shape) not in (1,2): return np.stack(b)
         ml = max(len(o) for o in b)
         if min(len(o) for o in b)==ml: return np.stack(b)
@@ -56,30 +60,30 @@ class DataLoader(object):
             else:            res[i,  :len(o)] = o
         return res
 
-    def np_collate(self, batch):
+    def np_collate_(self, batch):
         b = batch[0]
-        if isinstance(b, (np.ndarray, np.generic)): return self.jag_stack(batch)
+        if isinstance(b, (np.ndarray, np.generic)): return self.stack_jagged_(batch)
         elif isinstance(b, (int, float)): return np.array(batch)
         elif isinstance(b, string_classes): return batch
         elif isinstance(b, collections.Mapping):
-            return {key: self.np_collate([d[key] for d in batch]) for key in b}
+            return {key: self.np_collate_([d[key] for d in batch]) for key in b}
         elif isinstance(b, collections.Sequence):
-            return [self.np_collate(samples) for samples in zip(*batch)]
+            return [self.np_collate_(samples) for samples in zip(*batch)]
         raise TypeError(("batch must contain numbers, dicts or lists; found {}".format(type(b))))
 
-    def get_batch(self, indices):
-        res = self.np_collate([self.dataset[i] for i in indices])
-        if self.transpose:   res[0] = res[0].T
-        if self.transpose_y: res[1] = res[1].T
-        return res
 
-    def __iter__(self):
-        if self.num_workers==0:
-            for batch in map(self.get_batch, iter(self.batch_sampler)):
-                yield get_tensor(batch, self.pin_memory)
-        else:
-            with ThreadPoolExecutor(max_workers=self.num_workers) as e:
-                # avoid py3.6 issue where queue is infinite and can result in memory exhaustion
-                for c in chunk_iter(iter(self.batch_sampler), self.num_workers*10):
-                    for batch in e.map(self.get_batch, c): yield get_tensor(batch, self.pin_memory)
+string_classes = (str, bytes)
+
+
+def get_tensor_(batch, pin):
+    if isinstance(batch, (np.ndarray, np.generic)):
+        batch = T(batch).contiguous()
+        return batch.pin_memory() if pin else batch
+    elif isinstance(batch, string_classes): return batch
+    elif isinstance(batch, collections.Mapping):
+        return {k: get_tensor_(sample, pin) for k, sample in batch.items()}
+    elif isinstance(batch, collections.Sequence):
+        return [get_tensor_(sample, pin) for sample in batch]
+    raise TypeError("batch must contain numbers, dicts or lists; found {}"
+                     .format(type(batch)))
 
